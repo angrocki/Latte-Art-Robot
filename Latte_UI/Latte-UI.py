71c5386eea3414e75ebb652c633a8ba6d0cb8294
allyson-hur <allysonhur@gmail.com>
2022-12-11 20:17:36 -0500

    Added select feature to UI

--- Latte_UI/Latte-UI.py
+++ Latte_UI/Latte-UI.py
@@ -12,6 +12,7 @@ timer = 0
 WIDTH, HEIGHT = 1280, 720
 size = (WIDTH, HEIGHT)
 screen = pg.display.set_mode(size)
+home_background = pg.display.set_mode(size)
 color = (250, 243, 235)
 screen.fill(color)
 
@@ -58,22 +59,22 @@ class Button(pg.sprite.Sprite):
             y: 
                 An integer representing the y position of the button on the screen.
         '''
-        self._x = x
-        self._y = y
+        self._x = x - self._surface.get_width()/2
+        self._y = y - self._surface.get_height()/2
 
-        self.rect = self._surface.get_rect(topleft=(x,y))
+        self.rect = self._surface.get_rect(topleft=(self._x,self._y))
 
-    def set_rect(self, cen_x, cen_y):
-        '''
-        Sets the rectangular hitbox of the button.
+    # def set_rect(self, cen_x, cen_y):
+    #     '''
+    #     Sets the rectangular hitbox of the button.
 
-        Args:
-            cen_x: 
-                An integer representing the center x coordinate of the button.
-            cen_y: 
-                An integer representing the center y coordinate of the button.
-        '''
-        self.rect = self._surface.get_rect(topleft=(cen_x,cen_y))
+    #     Args:
+    #         cen_x: 
+    #             An integer representing the center x coordinate of the button.
+    #         cen_y: 
+    #             An integer representing the center y coordinate of the button.
+    #     '''
+    #     self.rect = self._surface.get_rect(topleft=(cen_x,cen_y))
 
     def scale(self,x_scale,y_scale):
         '''
@@ -87,6 +88,13 @@ class Button(pg.sprite.Sprite):
         '''
         self._surface = pg.transform.smoothscale(self._surface, (x_scale, y_scale))
 
+    def scale_gen(self, scale):
+
+        width =  self._surface.get_width()
+        height = self._surface.get_height()
+
+        self._surface = pg.transform.scale(self._surface, (int((width*scale)), int(height*scale)))
+
     def display(self, screen):
         '''
         Displays the buttons on the UI screen.
@@ -96,24 +104,44 @@ class Button(pg.sprite.Sprite):
                 The surface of the UI's background display.
         '''
         screen.blit(self._surface, (self._x, self._y))
-
+    
+    def draw(self):
+        action = False
+        if self.rect.collidepoint(pg.mouse.get_pos()):
+            if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+            if pg.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+        return action
 
 class DesignButton(Button):
     '''
     A subclass of the Button class that initializes the Latte design buttons 
     on the screen.
     '''
-    def __init__(self, x, y, type):
+    def __init__(self,type):
         '''
         Constructs all necessary attributes to initialize the button image.
         '''
-        super().__init__(x, y, type)
+        super().__init__(type)
 
         # Loads button display, depending on type of design
         if self._type == "Heart":
-            self._surface = pg.image.load("").convert_alpha()
+            self._surface = pg.image.load("UI_Images/buttons/Heart.png").convert_alpha()
         elif self._type == "Rosetta":
-            self._surface = pg.image.load("").convert_alpha() ##*****make folder for button images****
+            self._surface = pg.image.load("UI_Images/buttons/rosetta.png").convert_alpha() ##*****make folder for button images****
+        elif self._type == "Random":
+            self._surface = pg.image.load("UI_Images/buttons/random.png").convert_alpha()
+        elif self._type == "Selection_Circle":
+            self._surface = pg.image.load("UI_Images/buttons/Selection_Circle.png").convert_alpha()
+        elif self._type == "Checkbox":
+            self._surface = pg.image.load("UI_Images/buttons/checkbox.png").convert_alpha()
+        elif self._type == "Check":
+            self._surface = pg.image.load("UI_Images/buttons/check.png").convert_alpha()
+        
+        # Loads selection circles
+        
 
     def setup(self):
         '''
@@ -121,15 +149,14 @@ class DesignButton(Button):
         button's position on the screen.
         '''
         if self._type == "Heart":
-            self.set_position(100,20)
-            self.scale(95,95)
-            self.set_rect()
-            
+            self.set_position(800,875)
+            self.scale(425,350)
         if self._type == "Rosetta":
-            self.set_position(100,20)
-            self.scale(95,95)
-            self.set_rect()
-
+            self.set_position(1225,850)
+            self.scale(400,325)
+        if self._type == "Random":
+            self.set_position(1290,500)
+            self.scale(525,385)
 
 class NavigationButton(Button):
     '''
@@ -141,7 +168,7 @@ class NavigationButton(Button):
         Constructs all necessary attributes to initialize the button
         image.
         '''
-        super(NavigationButton, self).__init__(type)
+        super().__init__(type)
 
         # Loads button display, depending on type of navigation button
         if self._type == "Start":
@@ -153,7 +180,7 @@ class NavigationButton(Button):
         elif self._type == "Begin Drawing":
             self._surface = pg.image.load("UI_Images/buttons/start_button.png").convert_alpha()
         elif self._type == "Back":
-            self._surface = pg.image.load("button_temp.png").convert_alpha()
+            self._surface = pg.image.load("UI_Images/buttons/back_button.png").convert_alpha()
 
     def setup(self):
         '''
@@ -161,35 +188,99 @@ class NavigationButton(Button):
         prepare each button's position on the screen.
         '''
         if self._type == "Start":
-            self.set_position(540,400)
-            self.scale(200,200)
-            self.set_rect(540,400)
+            self.set_position(640,625)
         if self._type == "Home":
-            self.set_position(900,30)
-            self.scale(95,95)
+            self.set_position(640,625)
         if self._type == "Next":
-            self.set_position(540,400)
+            self.set_position(1100,625)
         if self._type == "Begin Drawing":
-            self.set_position(900,30)
-            self.scale(95,95)
+            self.set_position(640,625)
         if self._type == "Back":
-            self.set_position(900,30)
-            self.scale(95,95)
+            self.set_position(640,625)
 
+class SelectButton(Button):
+    def __init__(self,type):
+        super().__init__(type)
 
-# temp images 
+        if self._type == "Heart Border":
+            self._surface = pg.image.load("UI_Images/buttons/Selection_Circle.png").convert_alpha()
+        elif self._type == "Rosetta Border":
+            self._surface = pg.image.load("UI_Images/buttons/Selection_Circle.png").convert_alpha()
+        elif self._type == "Random Border":
+            self._surface = pg.image.load("UI_Images/buttons/Selection_Circle.png").convert_alpha()
+    def setup(self):
+        if self._type == "Heart Border":
+            self.set_position(525,590)
+            self.scale(1400,900)
+        if self._type == "Rosetta Border":
+            self.set_position(925, 590)
+            self.scale(1400,900)
+        if self._type == "Random Border":
+            self.set_position(1325, 590)
+            self.scale(1400,900)
+
+# naviation images 
 start_button = NavigationButton("Start")
 start_button.setup()
 
 home_button = NavigationButton("Home")
 home_button.setup()
 
+next_button = NavigationButton("Next")
+next_button.setup()
+
 begin_drawing_button = NavigationButton("Begin Drawing")
 begin_drawing_button.setup()
 
 back_button = NavigationButton("Back")
 back_button.setup()
+
 # startbut.display(screen)
+
+# design images
+heart = DesignButton("Heart")
+heart.setup()
+rosetta = DesignButton("Rosetta")
+rosetta.setup()
+random = DesignButton("Random")
+random.setup()
+
+heart_border = SelectButton("Heart Border")
+heart_border.setup()
+rosetta_border = SelectButton("Rosetta Border")
+rosetta_border.setup()
+random_border = SelectButton("Random Border")
+random_border.setup()
+
+class Background():
+    def __init__(self, type):
+
+        self._type = type
+        
+        if self._type == "Home":
+            self._surface = pg.image.load("UI_Images/background/homepage.png").convert_alpha()
+            self._surface = pg.transform.scale(self._surface, size)
+        elif self._type == "Choose":
+            self._surface = pg.image.load("UI_Images/background/choosePattern.png").convert_alpha()
+            self._surface = pg.transform.scale(self._surface, size)
+        elif self._type == "Checklist":
+            self._surface = pg.image.load("UI_Images/background/check_list.png").convert_alpha()
+            self._surface = pg.transform.scale(self._surface, size)
+        elif self._type == "Waiting":
+            self._surface = pg.image.load("UI_Images/background/waiting.png").convert_alpha()
+            self._surface = pg.transform.scale(self._surface, size)
+        elif self._type == "Done":
+            self._surface = pg.image.load("UI_Images/background/done.png").convert_alpha()
+            self._surface = pg.transform.scale(self._surface, size)
+
+
+
+# background images
+home_screen = Background("Home")
+choose_screen = Background("Choose")
+checklist_screen = Background("Checklist")
+waiting_screen = Background("Waiting")
+done_screen = Background("Done")
 
 
 class Menu():
@@ -201,6 +292,15 @@ class Menu():
             self.home()
         elif self.state == 'choose':
             self.choose()
+            print("switching")
+        elif self.state == 'choose_heart':
+            self.choose_heart()
+        elif self.state == 'choose_rosetta':
+            self.choose_rosetta()
+        elif self.state == 'choose_random':
+            self.choose_random()
+        elif self.state == 'checklist':
+            self.checklist()
         elif self.state == 'waiting':
             self.waiting()
         elif self.state == 'done':
@@ -217,22 +317,124 @@ class Menu():
             if event.type == pg.MOUSEBUTTONDOWN:
                 if start_button.rect.collidepoint(pg.mouse.get_pos()):
                     self.state = 'choose' 
-        
+                    print("CLICKED")
         pg.display.update()
+        home_screen.draw(screen)
         start_button.display(screen)
 
     def choose(self):
+        '''
+        Choose page
+        '''
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                pg.sys.exit()
+            # if event.type == pg.MOUSEBUTTONDOWN:
+            #     if heart.rect.collidepoint(pg.mouse.get_pos()):
+            #         self.state = 'choose_heart'
+            #     if rosetta.rect.collidepoint(pg.mouse.get_pos()):
+            #         self.state = 'choose_rosetta'
+            #     if random.rect.collidepoint(pg.mouse.get_pos()):
+            #         self.state = 'choose_random'
+            if heart.draw():
+                self.state = 'choose_heart'
+            if rosetta.draw():
+                self.state = 'choose_rosetta'
+            if random.draw():
+                self.state = 'choose_random'
+        pg.display.update()
+        choose_screen.draw(screen)
+        heart.display(screen)
+        rosetta.display(screen)
+        random.display(screen)
+        print("im here")
+
+    def choose_heart(self):
+        '''
+        Choose page with heart selected
+        '''
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                pg.sys.exit()
+            if heart.draw():
+                self.state = 'choose_heart'
+            if rosetta.draw():
+                self.state = 'choose_rosetta'
+            if random.draw():
+                self.state = 'choose_random'
+            if next_button.draw():
+                self.state = 'checklist'
+        pg.display.update()
+        choose_screen.draw(screen)
+        heart.display(screen)
+        heart_border.display(screen)
+        rosetta.display(screen)
+        random.display(screen)
+        next_button.display(screen)
+        
+            
+    def choose_rosetta(self):
+        '''
+        Choose page with rosetta selected
+        '''
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                pg.sys.exit()
+            if heart.draw():
+                self.state = 'choose_heart'
+            if rosetta.draw():
+                self.state = 'choose_rosetta'
+            if random.draw():
+                self.state = 'choose_random'
+            if next_button.draw():
+                self.state = 'checklist'
+        pg.display.update()
+        choose_screen.draw(screen)
+        heart.display(screen)
+        rosetta.display(screen)
+        rosetta_border.display(screen)
+        random.display(screen)
+        next_button.display(screen)
+
+    def choose_random(self):
+        '''
+        Choose page with random selected
+        '''
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                pg.sys.exit()
+            if heart.draw():
+                self.state = 'choose_heart'
+            if rosetta.draw():
+                self.state = 'choose_rosetta'
+            if random.draw():
+                self.state = 'choose_random'
+            if next_button.draw():
+                self.state = 'checklist'
+        pg.display.update()
+        choose_screen.draw(screen)
+        heart.display(screen)
+        rosetta.display(screen)
+        random.display(screen)
+        random_border.display(screen)
+        next_button.display(screen)
+        
+    def checklist(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 pg.sys.exit()
             if event.type == pg.MOUSEBUTTONDOWN:
-                if start_button.rect.collidepoint(pg.mouse.get_pos()):
-                    self.state = 'choose' 
-        pass
-        
+                if begin_drawing_button.rect.collidepoint(pg.mouse.get_pos()):
+                    self.state = 'waiting'
         pg.display.update()
-        start_button.display(screen)
+        checklist_screen.draw(screen)
+        begin_drawing_button.display(screen)
+
 
     def waiting(self):
         for event in pg.event.get():
@@ -242,6 +444,8 @@ class Menu():
             if event.type == pg.MOUSEBUTTONDOWN:
                 if start_button.rect.collidepoint(pg.mouse.get_pos()):
                     self.state = 'waiting' 
+        pg.display.update()
+        waiting_screen.draw(screen)
 
     def done(self):
         for event in pg.event.get():
@@ -253,6 +457,18 @@ class Menu():
                     self.state = 'done' 
 
 menu = Menu()
+class HomeScreen():
+    '''
+    A subclass of the Button class that initializes the Latte design buttons 
+    on the screen.
+    '''
+    def __init__(self,type):
+        '''
+        Constructs all necessary attributes to initialize the button image.
+        '''
+        super().__init__(type)
+        
+
 
 #----------------------------------------------------------------------------------
 ## UI LOOP
